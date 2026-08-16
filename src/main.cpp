646ac53c46fbde7dd27a10c3760131ab6777a4ed
FoXcodeZ <m.grabowy@int.pl>
2026-08-17 00:58:59 +0200

    Hello Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+static int windowWidth = 1024;
+static int windowHeight = 768;
+
+#define UNUSED(x) (void)(x)
+
+static void framebufferSizeCallback(GLFWwindow* window, const int width, const int height)
+{
+    UNUSED(window);
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "ModernGL", nullptr, nullptr);
+
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+
+
+    glfwMakeContextCurrent(window);
+
+    if (gladLoadGL(glfwGetProcAddress) == 0)
+    {
+        std::cerr << "Failed to initialize OpenGL loader" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
